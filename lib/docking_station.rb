18d850608e77c040c7d5c52b0ed0275528400aa0
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-03-20 17:40:13 +0000

    adding bike to dock and view bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 class DockingStation
+  def initialize
+    @bikearray = []
+  end
 
   def release_bike
     Bike.new
   end
 
-end
+  def add_bike(bike)
+    @bikearray = << bike
+  end
+
+  def view_bikes
+    @bikearray
+  end
