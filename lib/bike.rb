9a0328d805afe1e5c5eb420b0ab1dc48021e86ba
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-03-23 17:06:50 +0000

    in middle of break

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,6 @@
 class Bike
+attr_reader :working
+
   def initialize
     @working = true
   end
@@ -6,4 +8,21 @@ class Bike
   def working?
     @working
   end
+
+  def break!
+    # if @working == true
+    # else
+    #   return @working
+  end
+
+
 end
+
+bike1 = Bike.new
+puts "New Instance = #{bike1}"
+bike1 = bike1.working?
+puts "Returning after calling working? = #{bike1}"
+bike1 = bike1.break!
+puts "Should be broken = #{bike1}"
+bike1 = bike1.working?
+puts "Returning after calling working? = #{bike1}"
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,19 @@
 require 'bike'
 
 describe Bike do
-  it {should respond_to (:working?)}
+
+  it { is_expected.to respond_to :working? }
+
+  it 'expects bike to be working' do
+    expect(subject.working?).to be true
+  end
+
+  it { is_expected. to respond_to :break! }
+
+  it 'Allows a bike to become broken' do
+    bike1 = Bike.new
+    broken_bike = bike1.break!
+    expect (broken_bike@working == true)
+  end
+
 end
